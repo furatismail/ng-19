f9acc54c316c8d8e4e27091aedc2a3063dbd80b2
Furát Ismail <furat.ismail@cadcom.cz>
2024-12-09 14:10:37 +0100

    prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitleComponent } from './components/title/title.component';
+// import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-19';
+  condition = true;
 }
