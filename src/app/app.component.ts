696393ea4eedd566a733dfb09cbbe1c74295ee0c
Furát Ismail <furat.ismail@cadcom.cz>
2024-12-11 22:06:45 +0100

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
-
+import { MatCardModule } from '@angular/material/card'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, LayoutComponent],
+  imports: [RouterOutlet, RouterLink, LayoutComponent, MatButtonModule, MatListModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  
+
 }
